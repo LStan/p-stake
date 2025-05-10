6f2c70e5a9b85907e125a381ddce4867d6633fda
LStan <LStan@users.noreply.github.com>
2025-05-10 16:24:59 +0700

    init

---
+++ program/src/entrypoint.rs
@@ -0,0 +1,167 @@
+use pinocchio::{
+    account_info::AccountInfo, no_allocator, nostd_panic_handler, program_entrypoint,
+    program_error::ProgramError, pubkey::Pubkey, ProgramResult,
+};
+
+// This is the entrypoint for the program.
+program_entrypoint!(process_instruction);
+//Do not allocate memory.
+no_allocator!();
+// Use the no_std panic handler.
+nostd_panic_handler!();
+
+#[inline(always)]
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    // convenience so we can safely use id() everywhere
+    if *program_id != crate::ID {
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let (ix_disc, instruction_data) = instruction_data
+        .split_first_chunk::<4>()
+        .ok_or(ProgramError::InvalidInstructionData)?;
+
+    // Second variant, test CUs usage
+    // let (ix_disc, instruction_data) = instruction_data
+    //     .split_at_checked(4)
+    //     .ok_or(ProgramError::InvalidInstructionData)?;
+
+    let instruction = &ix_disc[0];
+
+    // TODO: add check for epoch_rewards_active
+    // let epoch_rewards_active = EpochRewards::get()
+    //         .map(|epoch_rewards| epoch_rewards.active)
+    //         .unwrap_or(false);
+    // if epoch_rewards_active && !matches!(instruction, StakeInstruction::GetMinimumDelegation) {
+    //     return Err(StakeError::EpochRewardsActive.into());
+    // }
+
+    match instruction {
+        // 0 - Initialize
+        0 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: Initialize");
+
+            todo!()
+        }
+        // 1 - Authorize
+        1 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: Authorize");
+
+            todo!()
+        }
+        // 2 - DelegateStake
+        2 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: DelegateStake");
+
+            todo!()
+        }
+        // 3 - Split
+        3 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: Split");
+
+            todo!()
+        }
+        // 4 - Withdraw
+        4 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: Withdraw");
+
+            todo!()
+        }
+        // 5 - Deactivate
+        5 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: Deactivate");
+
+            todo!()
+        }
+        // 6 - SetLockup
+        6 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: SetLockup");
+
+            todo!()
+        }
+        // 7 - Merge
+        7 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: Merge");
+
+            todo!()
+        }
+        // 8 - AuthorizeWithSeed
+        8 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: AuthorizeWithSeed");
+
+            todo!()
+        }
+        // 9 - InitializeChecked
+        9 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: InitializeChecked");
+
+            todo!()
+        }
+        // 10 - AuthorizeChecked
+        10 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: AuthorizeChecked");
+
+            todo!()
+        }
+        // 11 - AuthorizeCheckedWithSeed
+        11 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: AuthorizeCheckedWithSeed");
+
+            todo!()
+        }
+        // 12 - SetLockupChecked
+        12 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: SetLockupChecked");
+
+            todo!()
+        }
+        // 13 - GetMinimumDelegation
+        13 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: GetMinimumDelegation");
+
+            todo!()
+        }
+        // 14 - DeactivateDelinquent
+        14 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: DeactivateDelinquent");
+
+            todo!()
+        }
+        // 15 - Redelegate
+        15 => Err(ProgramError::InvalidInstructionData),
+        // 16 - MoveStake
+        16 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: MoveStake");
+
+            todo!()
+        }
+        // 17 - MoveLamports
+        17 => {
+            #[cfg(feature = "logging")]
+            pinocchio::msg!("Instruction: MoveLamports");
+
+            todo!()
+        }
+        _ => Err(ProgramError::InvalidInstructionData),
+    }
+}
---
+++ program/src/lib.rs
@@ -0,0 +1,5 @@
+#![cfg_attr(not(test), no_std)]
+
+mod entrypoint;
+
+pinocchio_pubkey::declare_id!("Stake11111111111111111111111111111111111111");
